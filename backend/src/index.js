be16f28669aad8d0c6bf10307430b1c66a6c5b28
Gaspar Lazarte <lazartegaspar@gmail.com>
2026-03-14 14:07:17 -0300

    Cambio en el cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io"
 import SocketManager from "./sockets/socketManager.js" // Importamos nuestro Gerente
 const PORT = process.env.PORT || 3000
 const io = new Server(PORT, {
-	cors: { origin: process.env.FRONTEND_URL || "http://localhost:5173" },
+	cors: { origin: "*" },
 })
 
 const socketManager = new SocketManager(io)
